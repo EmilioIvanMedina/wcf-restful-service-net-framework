8958494ccdd8bd5b91d5910b7e124e6473600882
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-10-15 23:11:25 -0300

    WCF - change service to REST architecture style

--- WcfRestfulService/WcfRestfulService/Services/IMovieSelector.cs
+++ WcfRestfulService/WcfRestfulService/Services/IMovieSelector.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 using WcfRestfulService.DataContracts;
 
@@ -13,6 +14,7 @@ namespace WcfRestfulService.Services
     public interface IMovieSelector
     {
         [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "MovieSelector/")]
         Movie SelectMovie();
     }
 }
