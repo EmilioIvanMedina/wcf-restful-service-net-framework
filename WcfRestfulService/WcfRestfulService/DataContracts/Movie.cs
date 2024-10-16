25db91c9a9e109604ad478a2ebe5b96b34d8b4f8
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-10-15 22:24:56 -0300

    WCF firs version of service is SOAP-based

---
+++ WcfRestfulService/WcfRestfulService/DataContracts/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace WcfRestfulService.DataContracts
+{
+    [DataContract]
+    public class Movie
+    {
+        [DataMember]
+        public int Year { get; set; }
+
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string Genre { get; set; }
+    }
+}
